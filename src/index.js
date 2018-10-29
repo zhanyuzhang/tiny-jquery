0b3d8c41d44cb3e536b01c9ee8b6bc913e9e4f96
chesszhang <1130142064@qq.com>
2018-10-29 09:32:14 +0800

    add some method

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-// 温馨提示：该库只提供dom相关的方法，其他方法请移除到其他库！谢谢
-function isPlainObject(value) {
-  return Object.prototype.toString.call(value, null) === '[object Object]';
-}
-
+// 温馨提示：该库只提供dom相关的方法，其他方法请移除到其他库！
 class Base {
   constructor(selector) {
     this.length = 0;
-    this.elements = Array.from(document.querySelectorAll(selector));
+    this.elements = Array.from(
+      selector.nodeName ? [selector] : document.querySelectorAll(selector)
+    );
     this.elements.forEach(elem => {
       [].push.call(this, elem);
     });
@@ -16,6 +14,7 @@ class Base {
       console.warn(`$(${selector}) doesn't match any elements！`);
     }
   }
+  // 样式相关方法
   hasClass(className) {
     return this.elements[0].classList.contains(className);
   }
@@ -31,29 +30,11 @@ class Base {
     });
     return this;
   }
-  html(args) {
-    if (args.length === 0) {
-      return this.elements[0].innerHTML;
-    }
-    this.elements.forEach(elem => {
-      elem.innerHTML = args[0];
-    });
-    return this;
-  }
-  text(args) {
-    if (args.length === 0) {
-      return this.elements[0].innerText;
-    }
-    this.elements.forEach(elem => {
-      elem.innerText = args[0];
-    });
-    return this;
-  }
   css(...args) {
     if (args.length === 1) {
       if (typeof args[0] === 'string') {
         return getComputedStyle(this.elements[0], null)[args[0]];
-      } else if (isPlainObject(args[0])) {
+      } else if (typeof args[0] === 'object') {
         const styleKeys = Object.keys(args[0]);
         this.elements.forEach(elem => {
           styleKeys.forEach(styleKey => {
@@ -69,8 +50,90 @@ class Base {
     }
     return this;
   }
+  show() {
+    this.elements.forEach(elem => {
+      elem.style.display = 'block';
+    });
+    return this;
+  }
+  hide() {
+    this.elements.forEach(elem => {
+      elem.style.display = 'none';
+    });
+    return this;
+  }
 
-  click() {}
+  // 内容相关的方法
+  html(...args) {
+    if (args.length === 0) {
+      return this.elements[0].innerHTML;
+    }
+    this.elements.forEach(elem => {
+      elem.innerHTML = args[0];
+    });
+    return this;
+  }
+  text(...args) {
+    if (args.length === 0) {
+      return this.elements[0].innerText;
+    }
+    this.elements.forEach(elem => {
+      elem.innerText = args[0];
+    });
+    return this;
+  }
+  val(...args) {
+    if (args.length === 0) {
+      return this.elements[0].value || '';
+    }
+    this.elements.forEach(elem => {
+      elem.value = args[0];
+    });
+    return this;
+  }
+
+  // 事件相关的方法
+  on(eventType, handler) {
+    if (typeof eventType === 'string' && typeof handler === 'function') {
+      this.elements.forEach(elem => {
+        elem.addEventListener(eventType, handler, false);
+      });
+    }
+    return this;
+  }
+  off(eventType, handler) {
+    if (typeof eventType === 'string' && typeof handler === 'function') {
+      this.elements.forEach(elem => {
+        elem.removeEventListener(eventType, handler, false);
+      });
+    }
+    return this;
+  }
+  click(handler) {
+    this.on('click', handler);
+  }
+
+  // 遍历方法
+  eq(index) {
+    if (index <= this.length) {
+      return new Base(this.elements[index]);
+    } else {
+      console.error(`eq(${index})超出索引范围`); // eslint-disabed
+    }
+  }
+
+  find(selector) {
+    return new Base(this.elements[0].querySelector(selector));
+  }
+
+  // 文档操作方法
+  append(elem) {}
 }
 
-export default selector => new Base(selector);
+function $(selector) {
+  return new Base(selector);
+}
+
+$.version = '1.0.0';
+
+export default $;
